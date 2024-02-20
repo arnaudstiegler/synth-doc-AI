7bd24af1f6e1f65f4b6a82150ce42ae43b8f6837
arnaudstiegler <arnaud.stiegler@gmail.com>
2024-02-20 08:44:16 -0500

    fix

--- dummy_train.py
+++ dummy_train.py
@@ -3,9 +3,11 @@ import torch
 from datasets import load_dataset
 from torch.optim import AdamW
 
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = AutoModelForCausalLM.from_pretrained(
     "microsoft/phi-2", torch_dtype=torch.float32, trust_remote_code=True
-)
+).to(device)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 
 dataset = load_dataset("squad")
@@ -13,13 +15,13 @@ dataset = load_dataset("squad")
 train_dataset = dataset["train"]
 optimizer = AdamW(model.parameters())
 
+sample = train_dataset[0]
+inputs_tokens = tokenizer(sample["question"], return_tensors="pt").to(device)
+answer_tokens = tokenizer(sample["answers"]["text"][0], return_tensors="pt").to(device)
 
 # TODO: overfitting on a single sample for now
-for sample in [train_dataset[0]]:
+for _ in range(100):
     optimizer.zero_grad()
-    inputs_tokens = tokenizer(sample["question"], return_tensors="pt")
-    answer_tokens = tokenizer(sample["answers"]["text"][0], return_tensors="pt")
-
     input_ids = torch.concat(
         [inputs_tokens["input_ids"], answer_tokens["input_ids"]], dim=-1
     )
@@ -29,10 +31,10 @@ for sample in [train_dataset[0]]:
             answer_tokens["input_ids"],
         ],
         dim=-1,
-    )
+    ).to(device)
 
     output = model(input_ids=input_ids, labels=labels)
     loss = output.loss
-    print(loss)
+    print(loss.item())
     loss.backward()
     optimizer.step()
