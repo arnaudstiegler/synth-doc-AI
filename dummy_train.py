c0bcbf964a03b65dc4958af58c82ebe2f52152b1
arnaudstiegler <arnaud.stiegler@gmail.com>
2024-02-20 09:01:10 -0500

    flash aantetion

--- dummy_train.py
+++ dummy_train.py
@@ -6,7 +6,9 @@ from torch.optim import AdamW
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = AutoModelForCausalLM.from_pretrained(
-    "microsoft/phi-2", torch_dtype=torch.float32, trust_remote_code=True
+    "microsoft/phi-2", torch_dtype='auto' if torch.cuda.is_available() else torch.float32,
+    trust_remote_code=True,
+    use_flash_attention_2=True
 ).to(device)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 
@@ -20,21 +22,22 @@ inputs_tokens = tokenizer(sample["question"], return_tensors="pt").to(device)
 answer_tokens = tokenizer(sample["answers"]["text"][0], return_tensors="pt").to(device)
 
 # TODO: overfitting on a single sample for now
-for _ in range(100):
-    optimizer.zero_grad()
-    input_ids = torch.concat(
-        [inputs_tokens["input_ids"], answer_tokens["input_ids"]], dim=-1
-    )
-    labels = torch.concat(
-        [
-            torch.tensor([-100] * inputs_tokens["input_ids"].shape[1], device=device).unsqueeze(0),
-            answer_tokens["input_ids"],
-        ],
-        dim=-1,
-    ).to(device)
+with torch.cuda.amp.autocast():
+    for _ in range(100):
+        optimizer.zero_grad()
+        input_ids = torch.concat(
+            [inputs_tokens["input_ids"], answer_tokens["input_ids"]], dim=-1
+        )
+        labels = torch.concat(
+            [
+                torch.tensor([-100] * inputs_tokens["input_ids"].shape[1], device=device).unsqueeze(0),
+                answer_tokens["input_ids"],
+            ],
+            dim=-1,
+        ).to(device)
 
-    output = model(input_ids=input_ids, labels=labels)
-    loss = output.loss
-    print(loss.item())
-    loss.backward()
-    optimizer.step()
+        output = model(input_ids=input_ids, labels=labels)
+        loss = output.loss
+        print(loss.item())
+        loss.backward()
+        optimizer.step()
