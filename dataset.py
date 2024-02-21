f26c134c139b1ed48b8e4f301909497a0da32b82
arnaudstiegler <arnaud.stiegler@gmail.com>
2024-02-21 08:29:31 -0500

    dummy dataset

--- dataset.py
+++ dataset.py
@@ -1,4 +1,40 @@
 from datasets import load_dataset
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+import torch
 
-dataset = load_dataset("squad")
 
+class SquadDataset(Dataset):
+    def __init__(self, tokenizer: AutoTokenizer, split: str):
+        # Upfront the cost of loading the entire dataset (not that big anyway)
+        self.samples = list(load_dataset('squad')[split])
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx: int):
+        sample = self.samples[idx]
+        inputs_tokens = self.tokenizer(sample['question'], return_tensors='pt')
+        answer_tokens = self.tokenizer(sample['answers']['text'][0], return_tensors='pt')
+
+        # Concatenate the question and the answer
+        input_ids = torch.concat(
+            [inputs_tokens["input_ids"], answer_tokens["input_ids"]], dim=-1
+        )
+        # Mask out the tokens from the question
+        labels = torch.concat(
+            [
+                torch.tensor([-100] * inputs_tokens["input_ids"].shape[1]).unsqueeze(
+                    0),
+                answer_tokens["input_ids"],
+            ],
+            dim=-1,
+        )
+        return {'input_ids': input_ids, 'labels': labels}
+
+
+if __name__ == '__main__':
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5")
+    dataset = SquadDataset(tokenizer, 'train')
+    import ipdb; ipdb.set_trace()
--- dummy_train.py
+++ dummy_train.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn.functional as F
 from datasets import load_dataset
 from accelerate import Accelerator
+from dataset import SquadDataset
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # model = AutoModelForCausalLM.from_pretrained(
@@ -25,78 +26,19 @@ model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1_5", torch_dtype=to
                                        attn_implementation="flash_attention_2").to(device)
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5")
 
-dataset = load_dataset("squad")
-
-train_dataset = dataset["train"]
-
-# with accelerator.main_process_first():
-#     # partial_collate_func = partial(collator, processor)
-#     # For Donut, we truncate to max length and can use the default collate
-#     train_dataloader = DataLoader(
-#         train_dataset,
-#         shuffle=True,
-#         batch_size=config["train_micro_batch_size_per_gpu"],
-#         # collate_fn=partial_collate_func
-#     )
-#
-# # TODO: overfitting on a single sample for now
-# with torch.cuda.amp.autocast():
-#     for _ in range(100):
-#         optimizer.zero_grad()
-#         input_ids = torch.concat(
-#             [inputs_tokens["input_ids"], answer_tokens["input_ids"]], dim=-1
-#         )
-#         labels = torch.concat(
-#             [
-#                 torch.tensor([-100] * inputs_tokens["input_ids"].shape[1], device=device).unsqueeze(
-#                     0),
-#                 answer_tokens["input_ids"],
-#             ],
-#             dim=-1,
-#         ).to(device)
-#
-#         output = model(input_ids=input_ids, labels=labels)
-#         loss = output.loss
-#         print(loss.item())
-#         loss.backward()
-#         optimizer.step()
+dataset = SquadDataset(tokenizer, 'train')
 
 
-
-accelerator = Accelerator()
-
-model = torch.nn.Transformer().to(device)
-optimizer = torch.optim.Adam(model.parameters())
-
-data = torch.utils.data.DataLoader(train_dataset, shuffle=True, batch_size=1)
-
+data = torch.utils.data.DataLoader(dataset, shuffle=True, batch_size=1)
+optimizer = AdamW(model.parameters())
 model, optimizer, data = accelerator.prepare(model, optimizer, data)
 
 model.train()
-
-inputs_tokens = tokenizer(train_dataset[0]['question'])
-answer_tokens = tokenizer(train_dataset[0]['answers'][0])
-
 for epoch in range(10):
-    for _ in data:
-
-        input_ids = torch.concat(
-        [inputs_tokens["input_ids"], answer_tokens["input_ids"]], dim=-1
-        )
-        labels = torch.concat(
-        [
-        torch.tensor([-100] * inputs_tokens["input_ids"].shape[1], device=device).unsqueeze(
-            0),
-        answer_tokens["input_ids"],
-        ],
-        dim=-1,
-        ).to(device)
-
+    for batch in data:
         optimizer.zero_grad()
 
-        output = model(source)
-        loss = F.cross_entropy(output, targets)
-
-        loss.backward()
+        output = model(batch['input_ids'], labels=batch['labels'])
+        loss = output.loss
         accelerator.backward(loss)
         optimizer.step()
