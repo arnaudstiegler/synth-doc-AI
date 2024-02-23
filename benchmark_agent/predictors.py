0009b4b5d89601ca8a9ee7bd2b26ce6187ea3030
arnaudstiegler <arnaud.stiegler@gmail.com>
2024-02-23 08:57:36 -0500

    add benchmark agent

---
+++ benchmark_agent/predictors.py
@@ -0,0 +1,58 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
+
+device = "cuda" if torch.cuda.is_available() else 'cpu'
+
+
+# TODO: should we force it to return a json dict?
+SYS_PROMPT = "You are an AI agent used for automation. Do not act like a chatbot"
+
+
+class Predictor:
+    def generate_answer(self, prompt: str):
+        raise NotImplementedError
+
+
+class MistralOpenOrcaPredictor(Predictor):
+    def __init__(self):
+        self.model = AutoModelForCausalLM.from_pretrained(
+            "Open-Orca/Mistral-7B-OpenOrca",
+            torch_dtype=torch.float16,
+            attn_implementation="flash_attention_2",
+            ).to(device)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "Open-Orca/Mistral-7B-OpenOrca")
+
+    def generate_answer(self, prompt: str):
+        prefix = "<|im_start|>"
+        suffix = "<|im_end|>\n"
+        sys_format = prefix + "system\n" + SYS_PROMPT + suffix
+        user_format = prefix + "user\n" + prompt + suffix
+        assistant_format = prefix + "assistant\n"
+        input_text = sys_format + user_format + assistant_format
+
+        generation_config = GenerationConfig(
+            max_length=256, temperature=1.1, top_p=0.95, repetition_penalty=1.0,
+            do_sample=True, use_cache=True,
+            eos_token_id=self.tokenizer.eos_token_id, pad_token_id=self.tokenizer.eos_token_id,
+        )
+
+        inputs = self.tokenizer(input_text, return_tensors="pt", return_attention_mask=True).to(device)
+        outputs = self.model.generate(**inputs, generation_config=generation_config)
+
+        return self.post_process_output(outputs)
+
+    def post_process_output(self, outputs):
+        start_index = torch.where(outputs == torch.tensor(self.tokenizer.encode('assistant', add_special_tokens=False)).to(device))[1]
+        return self.tokenizer.decode(outputs[0, start_index + 1:], skip_special_tokens=True).strip()
+
+
+class MistralInstructPredictor(Predictor):
+    def __init__(self):
+        self.model = AutoModelForCausalLM.from_pretrained(
+            "mistralai/Mistral-7B-v0.1",
+            torch_dtype=torch.float16,
+            attn_implementation="flash_attention_2",
+        ).to(device)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "mistralai/Mistral-7B-v0.1")
---
+++ benchmark_agent/run_eval.py
@@ -0,0 +1,14 @@
+import json
+import os
+from benchmark_agent.predictors import MistralOpenOrcaPredictor
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+samples = json.load(open(os.path.join(dir_path, 'tasks.json')))
+
+model_predictor = MistralOpenOrcaPredictor()
+
+for sample in samples:
+    prompt = sample['task_definition'] + ' ' + sample['task_input']
+    expected_answer = sample['expected_output']
+    generated_answer = model_predictor.generate_answer(prompt)
+    print(generated_answer, expected_answer)
