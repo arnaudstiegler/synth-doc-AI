ab1ace148b4b892d70874ffafc8b840a67dc5411
arnaudstiegler <arnaud.stiegler@gmail.com>
2024-05-23 09:43:02 -0400

    iteration

--- pali_gemma/debug_train.py
+++ pali_gemma/debug_train.py
@@ -9,6 +9,8 @@ import torch
 import json
 from datasets import load_dataset
 from transformers import AutoProcessor
+import bitsandbytes as bnb
+
 
 accelerator = Accelerator()
 device_index = accelerator.process_index
@@ -22,18 +24,12 @@ dataset = load_dataset("arnaudstiegler/synthetic_us_passports_easy")
 processor.max_length = 128
 image_token = processor.tokenizer.convert_tokens_to_ids("<image>")
 def collate_fn(examples):
-    texts = []
-    for example in examples:
-        text_str = ', '.join([f'{k.replace("_", " ")} = {v}' for k, v in example.items() if k != 'image'])
-        texts.append(text_str)
+    texts = ["Process " for example in examples]
+    labels= [json.dumps({k:v for k, v in example.items() if k != 'image'}) for example in examples]
     images = [example["image"].convert("RGB") for example in examples]
-    tokens = processor(text=texts, images=images,
-                return_tensors="pt", truncation=True, padding=True, max_length=128,
-                tokenize_newline_separately=False)
-    labels = tokens["input_ids"].clone()
-    labels[labels == processor.tokenizer.pad_token_id] = -100
-    labels[labels == image_token] = -100
-    tokens["labels"] = labels
+    tokens = processor(text=texts, images=images, suffix=labels,
+                    return_tensors="pt", truncation=True, padding=True, max_length=128,
+                    tokenize_newline_separately=False)
     return tokens
 
 
@@ -53,23 +49,34 @@ model = PaliGemmaForConditionalGeneration.from_pretrained(
     quantization_config=bnb_config, 
     device_map = {"": device_index}
 )
+model.gradient_checkpointing_enable()
 model = get_peft_model(model, lora_config)
 
-optimizer = torch.optim.Adam(model.parameters())
+optimizer = bnb.optim.Adam8bit(model.parameters(), lr=1e-4)
+# optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-data = torch.utils.data.DataLoader([dataset['train'][0]], shuffle=True, collate_fn=collate_fn, batch_size=1)
+data = torch.utils.data.DataLoader(dataset['train'], shuffle=True, collate_fn=collate_fn, batch_size=1)
 
 model, optimizer, data = accelerator.prepare(model, optimizer, data)
 
-model.train()
+
 for epoch in range(100):
     for batch in data:
+        model.train()
         optimizer.zero_grad()
         
         output = model(**batch)
         loss = output.loss
+
+        # This is an ugly fix
+        loss.requires_grad_(True)
         print(loss)
         
         accelerator.backward(loss)
-        
         optimizer.step()
+
+    # if epoch % 10 == 0:
+    #     with torch.no_grad():
+    #         model.eval()
+    #         out = model.generate(**batch, max_new_tokens=128)
+    #         print(f'epoch {epoch}', processor.batch_decode(out, skip_special_tokens=True))
