4e793679bbcc7bfdd70eb8b5452bd74d7d3283d6
arnaudstiegler <arnaud.stiegler@gmail.com>
2024-05-24 15:25:50 -0400

    test

--- pali_gemma/hf_trainer.py
+++ pali_gemma/hf_trainer.py
@@ -78,20 +78,25 @@ processor.max_length = 128
 
 
 bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_storage=torch.bfloat16,
 )
 
 lora_config = LoraConfig(
     r=8,
-    target_modules=[
-        "q_proj",
-        "o_proj",
-        "k_proj",
-        "v_proj",
-        "gate_proj",
-        "up_proj",
-        "down_proj",
-    ],
+    # target_modules=[
+    #     "q_proj",
+    #     "o_proj",
+    #     "k_proj",
+    #     "v_proj",
+    #     "gate_proj",
+    #     "up_proj",
+    #     "down_proj",
+    # ],
+    target_modules="all-linear",
     task_type="CAUSAL_LM",
 )
 model = PaliGemmaForConditionalGeneration.from_pretrained(
